10a60c26fc3ee7ce9573147693105ec421d4ac97
blackcub3s <santiago.sanchez.sans.44@gmail.com>
2025-03-25 17:14:32 +0100

    borrat front-end antic. Migrarr-lo en acabar el nou

--- APP WEB/__frontend__produccio__/landingPage/js/barraTopRedirigeixLogin.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var loginButton = document.getElementById("botoLogIn");
-    loginButton.addEventListener("click", function () {
-        window.location.href = "pas2C_login.html"; // Replace with the URL you want to redirect to
-    });
-});
