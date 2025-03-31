4ba6de2d442841d4655d4a7ec47b3cc309b3956e
blackcub3s <santiago.sanchez.sans.44@gmail.com>
2025-03-31 17:32:16 +0100

    token api/nreUusaris es securitzat per a USER, funciona b! Ara comprovar la securitzacio per a ADMIN i endpoins amb parametre a partir de idUsuari

--- APP WEB/__springboot__produccio__/app/src/main/java/miApp/app/seguretat/ConfiguracioSeguretat.java
+++ APP WEB/__springboot__produccio__/app/src/main/java/miApp/app/seguretat/ConfiguracioSeguretat.java
@@ -26,8 +26,8 @@ public class ConfiguracioSeguretat {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         //.requestMatchers("/api/usuaris").authenticated()  //ENDPOINT PROTEGIT
-                        .requestMatchers("/api/usuaris").hasRole("ADMIN") //ENDPOINT PROTEGIT
-                        .requestMatchers("/api/nreUsuaris").hasRole("USER") //ENDPOINT PROTEGIT
+                        .requestMatchers("/api/usuaris").hasRole("ADMIN")   //ENDPOINT PROTEGIT (ADMIN es permisos == 2 de la bbdd veure FiltreAutenticacioJwt)
+                        .requestMatchers("/api/nreUsuaris").hasRole("USER") //ENDPOINT PROTEGIT (USER es permisos==1 de la bbdd) TESTEJAT! VA BE I QUAN EXPIRA TOKEN PARA AACCES!
                         .requestMatchers("/api/**").permitAll()  // PERMET QUE LA RESTA D'ENDPOINTS dins /api/ SIGUIN PUBLICS
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
