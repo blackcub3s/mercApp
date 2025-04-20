405fa7cb3b26e59569a7edfdc660772f0e7a9d56
blackcub3s <santiago.sanchez.sans.44@gmail.com>
2025-04-20 15:32:14 +0200

    afegits nous tokens fins 12 de juny. Actu memoria. Canviat error en la programació de AccessToken

--- APP WEB/__springboot__produccio__/app/src/main/java/miApp/app/seguretat/jwt/AccessToken.java
+++ APP WEB/__springboot__produccio__/app/src/main/java/miApp/app/seguretat/jwt/AccessToken.java
@@ -12,7 +12,7 @@ import java.util.Map;
 @Component
 public class AccessToken extends JwtUtil {
 
-    private static int tExpM; //expiracio en minuts
+    private int tExpM; //expiracio en minuts
 
     public AccessToken() {
         this.tExpM = 1; //15 minuts
@@ -34,7 +34,7 @@ public class AccessToken extends JwtUtil {
                 .setClaims(dadesExtraApayload)   //dades customitzades
                 .setSubject(correu)              //guardo nom subjecte (clau "sub")
                 .setIssuedAt(new Date())         //data creacio (clau "iat" payload)
-                .setExpiration(new Date(System.currentTimeMillis() + (tExpM*60*1000)))  //(clau "exp")
+                .setExpiration(new Date((System.currentTimeMillis() / 1000 + (tExpM * 60)) * 1000))
                 .signWith(SignatureAlgorithm.HS256, clauSecreta.getBytes())
                 .compact();
     }
@@ -45,25 +45,40 @@ public class AccessToken extends JwtUtil {
     //PROVEM LA CLASSE
     public static void main(String[] args) {
 
-        //ES BYTE A LA BBDD, OKO
+        //ES BYTE A LA BBDD, OJO
         int permisos = 1;
 
-        //GENERO EL ACCESS TOKEN
+
         AccessToken accessToken = new AccessToken();
-        String accesJWT = accessToken.genera("santo@gmail.com",
+
+        //GENERO EL ACCESS TOKEN AMB PERMISOS D'ADMIN
+        String accesJWT_admin = accessToken.genera("superacces@gmail.com",
+                1, //id usuari
+                (byte) 2 //permisos d admin
+        );
+
+        //GENERO EL ACCESS TOKEN AMB PERMISOS d'USUARI
+        String accesJWT_user = accessToken.genera("acces@gmail.com",
                 2, //id usuari
-                (byte) permisos
+                (byte) 1   //permisos d'usuari
+        );
+
+        //GENERO EL TOKEN QUE S'EXPEDEIX AL QUE JA HA DONAT CORREU I CONTRA (PERO NO TENIM RES MES D ELL)
+        String accesJWT_mesBaix= accessToken.genera("noacces@gmail.com",
+                3, //id usuari
+                (byte) 0   //permisos mes baixos
         );
 
 
 
         //DONA EXCEPCIO SI EL TOKEN HA EXPIRAT, COMPTE!
-        System.out.println("token acces generat: \n"+accesJWT);
-
+        System.out.println("\ntoken acces generat (admin): \n"+accesJWT_admin);
+        System.out.println("\ntoken acces generat (user): \n"+accesJWT_user);
+        System.out.println("\ntoken acces generat (mesBaix): \n"+accesJWT_mesBaix);
 
         //PROVO D OBTENIR DADES DE LES CLAIMS (DELS PARELLS CLAU VALOR DEL PAYLOAD)
         try {
-            System.out.println(accessToken.getClaims(accesJWT).get("sub"));
+            System.out.println(accessToken.getClaims(accesJWT_admin).get("sub"));
             System.out.println(accessToken.getClaims("eyJhbGciOiJIUzI1NiJ9.eyJwZXJtaXNvcyI6MSwiaWRVc3VhcmkiOjIsInN1YiI6InNhbnRvQGdtYWlsLmNvbSIsImlhdCI6MTc0MzEwOTY4OCwiZXhwIjoxNzQzMTA5NzQ4fQ.D6pJVH88D6LlW2YKPkvAC5ZIkWLUboazjNbVOROCI3M").get("sub"));
         } catch (ExpiredJwtException e) {
             System.out.println("___Token Expirat___:        "+e.getMessage());
