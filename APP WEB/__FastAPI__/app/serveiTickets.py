1b451af27b39680b4ed16bb9ffa805da6e4a1b5b
Santi Sánchez Sans <107409217+blackcub3s@users.noreply.github.com>
2025-05-24 18:51:48 +0200

    Refactorització endpoint pujar tickets.  (#10)
    * funcio guardaRguardaTicketsAsistemaDarxius externalitzada a funcio de controlador a part sense endpoint: passarla a serveiTickets
    
    * moguda funcio a serveiTickets
    
    * actu memoria
    
    * afegit comentari pre post a guardaTicketsAsistemaDarxius
    
    * actu memoria

--- APP WEB/__FastAPI__/app/serveiTickets.py
+++ APP WEB/__FastAPI__/app/serveiTickets.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import os
 import PyPDF2
 import pytz
-
+from serveiValidacions import ticketValidat
 
 
 s = '' #on posaré l'string de la notificació
@@ -69,8 +69,64 @@ def esborra_pdfs(ll_esp, carregatPdfs):
             os.remove(pdf)
         
         
+#PRE: - llJudicis: una llista buida (passada per referència)
+#     - idUusari_enToken: un int que conté l'idUsuari obtingut del payload del token entrant.
+#     - arxius: una llista (list) que conté dades de tipus UploadFile
+#POST: 
+#     - llJudicis: passa per referència i s'emplena d'una llista de diccionaris (rollo JSON) que 
+#                  conté claus archivo, estado y tamany de cada arxiu
+#      - nRefusats: conté el nombre de tickets que no s'han guardat al servidor perquè o bé no compleixen el tamany o bé
+#                   no compleixen el títol que requereix la funció de validació ticketValidat() cridada.
+# --------------------------------------------------------------
+#        EXEMPLE llJudicis --> {"archivo": "20231004 Mercadona 27,40 €.pdf", "estado": "Guardado correctamente", "tamany": 35.7}
+async def guardaTicketsAsistemaDarxius(llJudicis, idUsuari_enToken, arxius):
+    
+    KB_MAXIM_TICKET_DIGITAL = 80 # empiricament el 2023 els tickets de mercadona pesaven 36KB
+    KB_MINIM_TICKET_DIGITAL = 15 # empiricament al 2025 els tickets pesaven 33KB (deixem marge)
+    
+    nRefusats = 0
+    for arxiu in arxius:
 
+        #AVALUA EL TIPUS MIME (BUSCO application/pdf)
+        if arxiu.content_type != "application/pdf": 
+            llJudicis.append({"archivo": arxiu.filename, "estado" : "No guardado! No es un PDF" })
+            nRefusats += 1 
+        
+        else:
+            # COMPROVEM EL TAMANY ABANS DE LLEGIR L'ARXIU EN MEMOÒRIA EN EL SERVIDOR
+            # l'atribut built-in de l'objecte UploadFile, .size,conté el tamany en bytes sense necessitat de llegir tot l'arxiu en memoria.
+            # evitem que algu ens puji un fitxer molt gran i ens peti el servidor.     
+            #        
+            tamanyFitxerKB = round(arxiu.size/1024, 1) # com que arxiu.size és un enter que torna el nombre de bytes que ocupa l'arxiu i el puc passar a KB.
+            nomArxiu = os.path.basename(arxiu.filename)
 
+            # --- SI I NO MÉS SI EL TAMANY DE FITXER ES CORRECTE EL FEM LLEGIR TOT I, ALESHORES SÍ, EL GUARDEM A LA CARPETA DEL SERVIDOR --
+            # NOTEU que python admet la sintaxi matemàtica de l'estil A <= B <= C. Ho podriem haver escrit també
+            # (A <= B) and (B <= C), que seria el que en altres llenguatges com Java o Javascript és (A <= B) && (B <= C) 
+            if KB_MINIM_TICKET_DIGITAL <= tamanyFitxerKB <= KB_MAXIM_TICKET_DIGITAL:
+                #Comprovo si el nom de l'arxiu és el que m'espero en un ticket de Mercadona
+                if ticketValidat(nomArxiu):
+                    #Ara sí que llegeixo en memoria tot l'arxiu dins el servidor-
+                    #await arxiu.read() funciona b per a pdfs petits (menys de 80 kb). arxiuBinari es tipus bytes.
+                    arxiuBinari = await arxiu.read() #ARA SÍ QUE LLEGEIXO EN MEMORIA TOT L'ARXIU
+
+                    #creo directori tickets si no existeix i trec el path
+                    directoriOnGuardar = f"./tickets/{idUsuari_enToken}"
+                    os.makedirs(directoriOnGuardar, exist_ok=True)
+                    path = os.path.join(directoriOnGuardar, nomArxiu) 
+
+                    #creo l'arxiu en mode d'escriptura (w) amb la b de binari i guardo dins l'arxiu binari
+                    with open(path, "wb") as f: 
+                        f.write(arxiuBinari)
+
+                    llJudicis.append({"archivo": nomArxiu, "estado": "Guardado correctamente", "tamany" : tamanyFitxerKB})
+                else:
+                    nRefusats += 1 #si nom no és correcte conto que és ticket refusat
+                    llJudicis.append({"archivo": nomArxiu, "estado": "Archivo rechazado y no guardado (NOMBRE incorrecto)!", "tamany" : tamanyFitxerKB})
+            else:
+                nRefusats += 1 #si tamany no és correcte conto que és ticket refusat
+                llJudicis.append({"archivo": nomArxiu, "estado": "Archivo rechazado y no guardado (tamaño incorrecto!)", "tamany" : tamanyFitxerKB})
+    return nRefusats
 
     
 
