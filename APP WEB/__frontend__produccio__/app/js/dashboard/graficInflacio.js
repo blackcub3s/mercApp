7387cef015e83d3c0d8231a84ab9fdc23ff487cb
blackcub3s <santiago.sanchez.sans.44@gmail.com>
2025-06-05 20:15:58 +0200

    refactor fetch en funcio separada. Si no va be el paginador en cridar a fesGraficProducte() aleshores torna a anterior COMMIT amb git reset Hard

--- APP WEB/__frontend__produccio__/app/js/dashboard/graficInflacio.js
+++ APP WEB/__frontend__produccio__/app/js/dashboard/graficInflacio.js
@@ -1,91 +1,89 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fesGraficProducte("producteMesOcurrent"); //EL DE LA PRIMERA ITERACIO REQUEREIX UN SETINTERVAL PER EVITAR PROBLEMES D'ASSINCRONIA
+});
+
+
+
+
+
+
+
+
 
 //AQUEST ITEM DE LOCAL STORAGE {'BOLSA PLASTICO': 132, 'BANANA': 53, 'BRONCHALES 6L': 50, .... , 'AGUA MINERAL': 1}        
 //NO ESTARÀ PLE FINS QUE LA CRIDA ASÍNCRONA A api/frequenciesProductes ESTIGUI RESOLTA. CADA 100 MILISEGONS
 //ESCANEGEM EL CONTINGUT DEL LOCAL STORAGE FINS TROBAR-LO.
+function fesFetch_graficDataPreuProducte(producteBuscat) {
+    const tokenAcces = localStorage.getItem("AccessToken");
+    fetch('http://localhost:8000/api/graficDataPreuProducte', {
+        method: "POST",
+        headers: {
+            'Authorization': "Bearer " + tokenAcces,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"nomProducte": producteBuscat})
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('La solicitud devolvió código de error en paso3 : ' + response.status + " || Mensaje de error: " + response.statusText);
+        }
+        return response.json();
+    })
+    .then(arrDataPreu => {
 
+        // LES DADES TINDRAN AQUEST ASPECTE ON x es la data i y es el preu. 
+        //let arrDataPreu = [{ "x": "2022-06-01", "y": 1 }, { "x": "2022-08-01", "y": 3 },{ "x": "2026-06-01", "y": 6 }];
+        fesGrafic(arrDataPreu); // FEM EL GRAFIC JA
+    })
+    .catch(error => {
+        console.error('Error en paso3:', error);
+    });
+}
 
-            
-
-        
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    //HEM DE FER UN SET ITNERVAL PORQUÈ DEL LOCAL STORAGE NO SABEM QUAN HI HAURAN LES DADES DINS.
+//FUNCIO QUE SI L IPASSEM EL NOM D'UN PRODUCTE ENS FA EL SEU GRAFIC 
+// (A EXCEPCIO DE SI LI PASSEM  producteMesOcurrent. Aleshores fa un fetch del prdocute més ocurrent tenint en compte que cal afegirt
+//un timeout per treure frequenciesProductes del localstorage; que s'havia rellenat d'una crida a endpoint i podria fallar si no mirem si ja esta ple.
+//la resta de crides vindran del paginador.
+function fesGraficProducte(nomProducte) {
+    //HEM DE FER UN SET INTERVAL PORQUÈ DEL LOCAL STORAGE NO SABEM QUAN HI HAURAN LES DADES DINS.
     //LA VARIABLE DEL LOCAL STORAGE QUE CONSULTEM AMB getItem ((let freqProductes = localStorage.getItem("frequenciesProductes");))
     //S'HAVIA EMPLENAT AMB LA SOLICITUD A l'endpoint /api/frequenciesProductes. D'ALLI EN TREIEM
     //LES CLAUS DE freqProductes, en l'ordre en el que van (ORDRE DE MÉS A MENYS APARICIÓ) IDEAL
     //PER A MOSTRAR ELS PRODUCTES EN EL GRÀFIC: PRIMER ELS QUE TENEN MÉS OCURRÈNCIA (PER ALS QUE EL GRÀFIC TÉ SENTIT)
     //I DESPRÉS AMB EL PAGINADOR ANIREM ACCEDINT ALS QUE TENEN MENYS OCURRÈNCIA I PERMETEN, PER TANT,
     //OBSERVAR PITJOR LA TENDÈNCIA D'INCREMENT DE PREUS
-    const idIntervalDashboard = setInterval(() => {
 
-        let freqProductes = localStorage.getItem("frequenciesProductes");
+    
 
-        if (freqProductes !== null) {
-            //QUAN JA HEM TROBAT QUE S'HA EMPLENAT ALESHORES JA PODEM PRENDRE LES DADES
-            clearInterval(idIntervalDashboard);
+    if (nomProducte == "producteMesOcurrent") { //EL PRODUCTE MES OCURRENT NO SEMPRE VE PER PARAMETRE
 
-            const tokenAcces = localStorage.getItem("AccessToken");
+        const idIntervalDashboard = setInterval(() => {
+            let freqProductes = localStorage.getItem("frequenciesProductes");
+            if (freqProductes !== null) {
+                clearInterval(idIntervalDashboard);//QUAN JA HEM TROBAT QUE S'HA EMPLENAT ALESHORES JA PODEM PRENDRE LES DADES
 
-            //DESSERIALITZEM EL JSON QUE CONTE PARELL PRODUCTE I FREQUENCIA DE COMPRA 
-            //  AIXI ---> {'BOLSA PLASTICO': 132, 'BANANA': 53, 'BRONCHALES 6L': 50, .... , 'AGUA MINERAL': 1}
-            let freqProductes = JSON.parse(localStorage.getItem("frequenciesProductes"));    
-
-            const clauProductes = Object.keys(freqProductes);   //clauProductes conté els productes ordenats de MÉS A MENYS compres
-            const producteMesComprat = clauProductes[0];        //el producrte mes comprat es BOLSA PLASTICO i quan carreguem la pagina es el primer grafi que mostrem
-            
-            
-            //const producteMesComprat = "POLLO ENTERO LIMPIO"; // PROVES QUE HEM FET
-            
-            fetch('http://localhost:8000/api/graficDataPreuProducte', {
-                method: "POST",
-                headers: {
-                    'Authorization': "Bearer " + tokenAcces,
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({"nomProducte": producteMesComprat})
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('La solicitud devolvió código de error en paso3 : ' + response.status + " || Mensaje de error: " + response.statusText);
-                }
-                return response.json();
-            })
-            .then(arrDataPreu => {
-                //CRIDEM A LA FUNCIÓ DE FER EL GRÀFIC
-
-                /*
-                    // LES DADES TINDRAN AQUEST ASPECTE ON x es la data i y es el preu. 
-                    // Tretes una per una amb solicitud post
-                    let arrDataPreu = [
-                        { "x": "2022-06-01", "y": 1 },
-                        { "x": "2022-08-01", "y": 3 },
-                        { "x": "2026-06-01", "y": 6 }
-                    ];
-                */
-
-                fesGrafic(arrDataPreu);
-            })
-            .catch(error => {
-                console.error('Error en paso3:', error);
-            });
+                //DESSERIALITZEM EL JSON QUE CONTE PARELL PRODUCTE I FREQUENCIA DE COMPRA    AIXI ---> {'BOLSA PLASTICO': 132, 'BANANA': 53, 'BRONCHALES 6L': 50, .... , 'AGUA MINERAL': 1}
+                let freqProductes = JSON.parse(localStorage.getItem("frequenciesProductes"));    
+                const clauProductes = Object.keys(freqProductes);   //clauProductes conté els productes ordenats de MÉS A MENYS compres
+                
+                
+                //el producrte mes comprat es BOLSA PLASTICO i quan carreguem la pagina es el primer grafi que mostrem. ES MOLT IMPORTANT FER-L UN TRACTE ESPECIAL
+                // PER AIXO HO POSEM DINS producteMesOcurrent. Es a primera carrega a la pagina i podria donar error per asincronia.
+                const producteMesComprat = clauProductes[0];        
+                
+                fesFetch_graficDataPreuProducte(producteMesComprat);
+            }
+        }, 200);
+    } else { //AQUEST PRODUCTES (2N PRODUCTE MES OCURRENT I SEGUENTS VINDRAN PER PARMETRE, DEL PAGINADOR.
+        fesFetch_graficDataPreuProducte(nomProducte);
+    }
+ 
+}
 
 
 
 
-
-        }
-        
-    }, 200);
-
-
-
-
-
-});
 
 function fesGrafic(arrDataPreu) {
         const ctx = document.getElementById('myChart').getContext('2d');
