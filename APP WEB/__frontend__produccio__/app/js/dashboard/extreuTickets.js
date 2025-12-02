be28747a3dc7a284889729621393ecf222e77bc9
blackcub3s <santiago.sanchez.sans.44@gmail.com>
2025-12-02 18:39:25 +0100

    feature: primera aproximació al roblema: només les barres afegides i sistema variable d'altures ideat

--- APP WEB/__frontend__produccio__/app/js/dashboard/extreuTickets.js
+++ APP WEB/__frontend__produccio__/app/js/dashboard/extreuTickets.js
@@ -116,7 +116,7 @@ document.addEventListener("DOMContentLoaded", () => {
 //global!
 function creaIrellenaBarres_histograma_intervalizer(llTickets) {
     const contenidorBarres = document.getElementById("wrapperBarres");
-    contenidorBarres.innerHTML = "FUNCIONO"; //buido barres de nesis en les cards, si en queden d'anteriors cerques (potser no cal)
+    //contenidorBarres.innerHTML = "FUNCIONO"; //buido barres de nesis en les cards, si en queden d'anteriors cerques (potser no cal)
                                                         //               aaaa.mm : 
     oMesos = computaAgregatPerMes_NONUL(llTickets); //a oMesos --> {"2025-3: 342, 2025-4: 212, etc.}". Mesos sense gasto NO APAREIXEN en CLAU ni en valor (0)-
     console.log(oMesos);
